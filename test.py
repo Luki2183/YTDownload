d9b610bc8c52285e05bc032705b494eeec180c96
Luki2183 <lenternus@gmail.com>
2024-12-26 12:20:06 +0100

    deleted last project
    starts pytubefix

--- test.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-print(os.getcwd())
-print(__file__)
-print(os.path.abspath(__file__))
-print(os.path.dirname(os.path.realpath(__file__)))
-print(sys.path[0])
-print(os.getenv('APPDATA'))
