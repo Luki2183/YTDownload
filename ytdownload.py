be10afaed5a5713f70a9884b19bd8f109b43e586
Luki2183 <lenternus@gmail.com>
2025-01-06 16:41:32 +0100

    Increased font size
    font size increase
    window resize
    window name change

--- ytdownload.py
+++ ytdownload.py
@@ -1,7 +1,40 @@
-import os, msvcrt, re
+import os, msvcrt, re, ctypes, pygetwindow, tkinter
 from pytubefix import Playlist, YouTube, helpers, innertube
 from pytubefix.exceptions import VideoUnavailable, AgeRestrictedError
+# Font and size stuff
+LF_FACESIZE = 32
+STD_OUTPUT_HANDLE = -11
 
+class COORD(ctypes.Structure):
+    _fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+class CONSOLE_FONT_INFOEX(ctypes.Structure):
+    _fields_ = [("cbSize", ctypes.c_ulong),
+                ("nFont", ctypes.c_ulong),
+                ("dwFontSize", COORD),
+                ("FontFamily", ctypes.c_uint),
+                ("FontWeight", ctypes.c_uint),
+                ("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+
+font = CONSOLE_FONT_INFOEX()
+font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+font.nFont = 12
+font.dwFontSize.X = 11
+font.dwFontSize.Y = 32
+font.FontFamily = 54
+font.FontWeight = 700
+font.FaceName = "Liberation Mono"
+
+handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+ctypes.windll.kernel32.SetCurrentConsoleFontEx(
+        handle, ctypes.c_long(False), ctypes.pointer(font))
+os.system("title YTDownload")
+window = pygetwindow.getWindowsWithTitle('YTDownload')[0]
+window.size = (1280, 360)
+# ---
+
+
+# Managing dir's
 innertube._cache_dir = os.path.join(os.getenv('APPDATA'), "YTDownloadCache")
 innertube._token_file = os.path.join(innertube._cache_dir, 'tokens.json')
 
@@ -18,6 +51,7 @@ else:
     directory = open(settingsdirectory, "x")
     download_directory = ''
 directory.close()
+# ---
 
 
 def cls():
